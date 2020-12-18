6c6458d1bab36916afd0f2b771b9f2d770bc95f7
Jose Barrios <jbarriospd@gmail.com>
2020-12-18 17:40:49 -0500

    [Add] Img in svg (2)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
 // import { useContext } from 'react';
 // import StoreContext from '../../store';
+import ComputerDesk from '../../assets/img/female';
 
 const Home = () => {
   // const { globalState, dispatch } = useContext(StoreContext);
 
   return (
-    <div>
-      Home
-    </div>
+    
+    <ComputerDesk />  
+    
   );
 };
 
