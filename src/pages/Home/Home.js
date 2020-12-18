c00f88913ff91720e40dda3106efbdd3fb436dcf
Jose Barrios <43865749+jbarriospd@users.noreply.github.com>
2020-12-18 17:57:34 -0500

    Revert "[Add] Img in svg (2)"

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,14 @@
 import React from 'react';
 // import { useContext } from 'react';
 // import StoreContext from '../../store';
-import ComputerDesk from '../../assets/img/female';
 
 const Home = () => {
   // const { globalState, dispatch } = useContext(StoreContext);
 
   return (
-    
-    <ComputerDesk />  
-    
+    <div>
+      Home
+    </div>
   );
 };
 
