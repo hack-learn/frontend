327ac2e461455c417bcbba7898d5604b092444e8
juan sebastian herrera <juan.herreraca@campusucc.edu.co>
2020-12-19 11:11:07 -0500

    feature: home/styles

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { header } from '@material-ui/core';
 import logo from '../../assets/img/logo.png';
 import women from '../../assets/img/women.png';
 import search from '../../assets/img/search.png';
+import background from '../../assets/img/background.png';
 import { Link } from 'react-router-dom';
 // import { useContext } from 'react';
 // import StoreContext from '../../store';
@@ -9,45 +11,87 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   // const { globalState, dispatch } = useContext(StoreContext);
 
+
   return (
-    <div>
+    
+    <div style= {{
+      margin: '0 auto',
+      padding: '20px',
+      width: '100%',
+      height: '100%',
+      fontFamily: 'Helvetica',
+      backgroundColor: '#E5E5E5'
+    }}>
         <div>
-        <figure>
-            <img src= { logo } alt="logo" />
+          <figure>
+            <img src= { logo }  alt="logo" />
           </figure>
         </div>
-        <div>
-          <h1>¿CONOCE TODO LO QUE
+        <div style= {{
+          marginTop: '50px'
+        }}>
+          <h1 style= {{
+            marginBottom: '20px',
+            width: '50%'
+          }}>¿CONOCE TODO LO QUE
               NECESITA PARA TU RUTA
               DE APRENDIZAJE EFECTIVA?
           </h1>
-          <p>
+          <p style= {{
+              width: '50%',
+              marginBottom: '50px'
+          }}>
           Nuestro sistema de diagnostico te ayuda a 
           orientar tus objetivos, especialidad e intereses 
           para que tu aprendizaje sea significativo.
           </p>
+          
           <Link to="/basic-information">COMENZAR AHORA</Link>
         </div>
         <div>
-          <figure>
+          <figure style= {{
+            position: 'absolute',
+            width: '254px',
+            height: '353px',
+            left: '1059px',
+            top: '100px'
+          }}>
             <img src= { women } alt="home-women" />
           </figure>
         </div>
+        
         <section>
-          <div>
+          <div style= {{
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '100px',
+            marginBottom: '20px'
+          }}>
             <h1>TOMA EL CONTROL DE TUS ESTUDIOS EN 2 PASOS</h1>
           </div>
-          <div>
-              <div>
-              <figure>
+          <div style= {{
+            display: 'flex',
+          }}>
+              <div style= {{
+                padding: '20px',
+                textAlign: 'center'
+              }}>
+              <figure style= {{
+                 marginBottom: '20px'
+              }}>
                 <img src= { search } alt="search" />
               </figure>
               <p>1. Escoge tu especialidad en Master y 
                     completa tu perfil para obtener tu diagnostico.
               </p>
               </div>
-              <div>
-              <figure>
+              <div style= {{
+                padding: '20px',
+                textAlign: 'center'
+              }}>
+              <figure style= {{
+                marginBottom: '20px'
+              }}>
                 <img src= { search } alt="search" />
               </figure>
               <p>2. Crearemos tu Learning Path para lograr
@@ -56,11 +100,7 @@ const Home = () => {
               </p>
               </div>
           </div>
-
         </section>
-  
-
-  
     </div>
     
   );
---
+++ src/pages/Traicing/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import { useContext } from 'react';
+// import StoreContext from '../../store';
+
+const Traicing = () => {
+  // const { globalState, dispatch } = useContext(StoreContext);
+  return (
+    
+    <div>
+        <table></table>
+    </div>
+    
+  );
+};
+
+export default Traicing;
